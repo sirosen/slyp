68fec1eb1ce1c844d143a1752352a4e32245feb8
Stephen Rosen <sirosen0@gmail.com>
2026-09-06 01:17:46 -0500

    Add simple missing tests

--- tests/checkers/test_basics.py
+++ tests/checkers/test_basics.py
@@ -48,6 +48,32 @@ def test_can_disable_code_with_comment(check_text):
     assert res.success
 
 
+def test_disable_requires_exact_format(check_text):
+    res = check_text(
+        """\
+        def foo():
+            x = "a " "b"  # slyp disable=E100
+        """,
+        filename="foo.py",
+    )
+    assert not res.success
+    assert "foo.py:2: unnecessary string concat (E100)" in res.message_strings
+
+
+def test_global_enable_overrides_global_disable(check_text):
+    res = check_text(
+        """\
+        def foo():
+            x = "a " "b"
+        """,
+        filename="foo.py",
+        enabled_codes={"E100"},
+        disabled_codes={"all"},
+    )
+    assert not res.success
+    assert "foo.py:2: unnecessary string concat (E100)" in res.message_strings
+
+
 def test_can_disable_code_via_category_disable(check_text):
     res = check_text(
         """\
