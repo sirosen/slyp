85e98d9974014082864983b56a8d0da146dc9f32
Stephen Rosen <sirosen0@gmail.com>
2026-09-12 17:30:07 -0500

    Enable sqlite WAL mode
    WAL results in significantly more efficient writes, especially given the
    many-small-writes which happen when populating an empty cache.

--- src/slyp/sqlite_cache/_initializer.py
+++ src/slyp/sqlite_cache/_initializer.py
@@ -31,7 +31,9 @@ class CacheInitializer:
             gitignore_path.write_text("*\n")
 
     def create_writer_connection(self) -> sqlite3.Connection:
-        return sqlite3.connect(str(self.filepath))
+        conn = sqlite3.connect(str(self.filepath))
+        conn.executescript("PRAGMA journal_mode=WAL;")
+        return conn
 
     def create_reader_connection(self) -> sqlite3.Connection:
         return sqlite3.connect(str(self.filepath))
